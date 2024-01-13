5f3f35b2f1935e4edf5182c18832bfe4565df7b8
adebayo-moses <adebayomoses597@gmail.com>
2024-01-13 23:43:12 +0100

    update

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,21 @@
 //@desc Get all Contacts
 //@route GET /api/contacts
 //@access Public
-
 const getContacts = (req, res) => {
-    res.status(200).json({message:'No contacts yet'});
+    res.status(200).json({message:'Get all contacts'});
+};
+
+//@desc Create new Contacts
+//@route POST /api/contacts
+//@access Public
+const cresteContact = (req, res) => {
+    res.status(201).json({message:'create contact'});
 }
+
+//@desc Get all Contacts
+//@route GET /api/contacts
+//@access Public
+const getContacts = (req, res) => {
+    res.status(200).json({message:`Get contact for ${req.params.id}`});
+}  
+module.exports = {getContacts};
